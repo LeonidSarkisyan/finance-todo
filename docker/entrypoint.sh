77ee450681eae5bf37e65bdc4039ef08f94dca6a
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2023-11-20 21:16:50 +0300

    Update dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,7 @@
-#! /bin/sh
+#!/bin/bash
 
-alembic upgrade HEAD
+alembic upgrade head
 
 cd src
 
 gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
-
-exec "$@"
