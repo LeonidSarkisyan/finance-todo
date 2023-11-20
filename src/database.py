f9844d1fc080da387bab94f6830024b072ad227e
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2023-11-20 20:51:46 +0300

    Update docker

--- src/database.py
+++ src/database.py
@@ -8,11 +8,9 @@ from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-DATABASE_URL_ = "postgresql+asyncpg://finance_todo_user:FtiTu1DgATmJs4IP2UDcqsGWEd8uQLKp@dpg-clcutufgsrdc73fhkna0-a/finance_todo"
-
 Base = declarative_base()
 
-engine = create_async_engine(DATABASE_URL_)
+engine = create_async_engine(DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
