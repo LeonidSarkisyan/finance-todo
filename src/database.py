10dc6a3a056ddaf376876ab534b96bd16e1a1e61
LeonidSarkisyan <TOPsarkisyanTOP@yandex.ru>
2023-11-20 20:24:11 +0300

    Update docker

--- src/database.py
+++ src/database.py
@@ -8,6 +8,10 @@ from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+print('-' * 100)
+print(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
+print('-' * 100)
+
 Base = declarative_base()
 
 engine = create_async_engine(DATABASE_URL)
